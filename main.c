6bca9c8ff11a3598873e0cf5d5be0378cee52826
Gerard <gerard.dziwinski@infotech.edu.pl>
2024-05-21 13:03:00 +0200

    reset board i zaczęcie prac nad drawBoard

--- main.c
+++ main.c
@@ -13,5 +13,22 @@ int main() {
     char player = ' ';
     char computer = ' ';
 
+    resetBoard();
+
+    drawBoard();
+
     return 0;
+}
+
+void resetBoard(){
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 8; j++){
+            board[i][j] = ' ';
+        }
+    }
+}
+
+void drawBoard(){
+    printf("| %c | %c | %c | %c | %c | %c | %c | %c |\n", board[0][0], board[0][1], board[0][2], board[0][3], board[0][4], board[0][5], board[0][6], board[0][7]);
+    printf("---------------------------------\n");
 }
