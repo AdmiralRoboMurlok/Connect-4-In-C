b0e48b8d1011b3b852a1afc14cd3b9a78260d844
Gerard <gerard.dziwinski@infotech.edu.pl>
2024-05-21 12:44:13 +0200

    Deklaracja funkcji i zmiennych

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+char board[4][8];
+
+void resetBoard();
+void drawBoard();
+int possibleMoves();
+void playerMove();
+void computerMove();
+char checkWinner();
+
+int main() {
+    char player = ' ';
+    char computer = ' ';
+
+    return 0;
+}
