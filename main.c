b7f0deb8f232636a2bf75f51c4d5fa34a5e152e6
Gerard <gerard.dziwinski@infotech.edu.pl>
2024-05-22 12:59:26 +0200

    dokończono planesze, oraz dodano player move

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 
 char board[4][8];
 
+const char PLAYER = '*';
+const char computer =  'O';
+
 void resetBoard();
 void drawBoard();
 int possibleMoves();
@@ -10,12 +13,15 @@ void computerMove();
 char checkWinner();
 
 int main() {
-    char player = ' ';
-    char computer = ' ';
+    char winner = ' ';
 
     resetBoard();
 
-    drawBoard();
+    while (winner == ' ' && possibleMoves() != 0){
+        drawBoard();
+
+        playerMove();
+    }
 
     return 0;
 }
@@ -31,4 +37,42 @@ void resetBoard(){
 void drawBoard(){
     printf("| %c | %c | %c | %c | %c | %c | %c | %c |\n", board[0][0], board[0][1], board[0][2], board[0][3], board[0][4], board[0][5], board[0][6], board[0][7]);
     printf("---------------------------------\n");
+    printf("| %c | %c | %c | %c | %c | %c | %c | %c |\n", board[1][0], board[1][1], board[1][2], board[1][3], board[1][4], board[1][5], board[1][6], board[1][7]);
+    printf("---------------------------------\n");
+    printf("| %c | %c | %c | %c | %c | %c | %c | %c |\n", board[2][0], board[2][1], board[2][2], board[2][3], board[2][4], board[2][5], board[2][6], board[2][7]);
+    printf("---------------------------------\n");
+    printf("| %c | %c | %c | %c | %c | %c | %c | %c |\n", board[3][0], board[3][1], board[3][2], board[3][3], board[3][4], board[3][5], board[3][6], board[3][7]);
+    printf("---------------------------------\n");
+    printf("  1   2   3   4   5   6   7   8\n");
+}
+
+int possibleMoves(){
+   int total = 0;
+
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 8; j++){
+            if(board[i][j] == ' '){
+                total++;
+            }
+        }
+    }
+}
+
+void playerMove(){
+    int mv;
+
+    printf("\n Wybierz kolumnę: ");
+    scanf("%d", &mv);
+    mv--;
+
+    for (int i = 0; i <= 4; i++){
+        if (board[4 - i][mv] == ' '){
+            board[4 - i][mv] = PLAYER;
+            break;
+        }
+    }
+}
+
+char checkWinner(){
+
 }
